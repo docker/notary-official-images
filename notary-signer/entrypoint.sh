3ea5b74928929415f2faaa5df574c4745bf6d286
David Lawrence <david.lawrence@docker.com>
2016-02-26 13:49:06 -0800

    official image consistency Signed-off-by: David Lawrence <david.lawrence@docker.com> (github: endophage)

---
+++ notary-signer/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+# this if will check if the first argument is a flag
+# but only works if all arguments require a hyphenated flag
+# -v; -SL; -f arg; etc will work, but not arg1 arg2
+if [ "${1:0:1}" = '-' ]; then
+    set -- notary-signer "$@"
+fi
+
+# else default to run whatever the user wanted like "bash"
+exec "$@"
